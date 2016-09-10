a98a5c7dde80c74947dd4dd44c6cb4aece31d8cc
Anna Tsolakou <tsolakou.a@gmail.com>
2016-09-10 21:02:36 +0200

    Create aVeryBigSum.go

---
+++ Algorithms/Warmup/aVeryBigSum.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main() {
+	
+		  var n, sum int
+		  fmt.Scan(&n)
+		  my_slice := make([]int, n)
+
+		  for i:=0; i< len(my_slice); i++ {
+	        fmt.Scanf("%d", &my_slice[i])
+	      }
+
+	      for i:=0; i< len(my_slice); i++ {
+	         sum = sum + my_slice[i]
+	      }
+
+	      fmt.Println(sum)
+}
