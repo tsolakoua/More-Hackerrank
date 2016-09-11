4ba233e2c87a010ba353c5b7c70af3a87393250c
Anna Tsolakou <tsolakou.a@gmail.com>
2016-09-11 12:57:18 +0200

    Create diagonialDifference.go

---
+++ Algorithms/Warmup/diagonialDifference.go
@@ -0,0 +1,34 @@
+package  main
+import "fmt"
+import "math"
+
+func main() {
+	var n, sumA, sumB int
+	fmt.Scan(&n)
+
+    myMatrix := make([][]int, n)
+    for k := 0; k < len(myMatrix); k++ {
+        myMatrix[k] = make([]int, n)
+    }
+    
+    for i := 0 ; i < len(myMatrix) ; i ++  {
+        for j := 0 ; j < len(myMatrix) ; j ++ {            
+            fmt.Scanf("%d", &myMatrix[i][j])
+        }
+    }
+
+    for i := 0 ; i < len(myMatrix) ; i ++  {
+        for j := 0 ; j < len(myMatrix) ; j ++ {
+                if i == j {
+                     sumA = sumA + myMatrix[i][j]
+                }
+            if i+j == n-1 {
+                sumB = sumB + myMatrix[i][j]
+            }
+            
+        }
+    }
+
+    fmt.Println(math.Abs(float64(sumA-sumB)))
+
+}
