6372bed219db49c5c34eaad678514b0bd6b7cefc
Anna Tsolakou <tsolakou.a@gmail.com>
2016-09-12 10:20:03 +0200

    Update aVeryBigSum.go

--- Algorithms/Warmup/aVeryBigSum.go
+++ Algorithms/Warmup/aVeryBigSum.go
@@ -3,17 +3,17 @@ import "fmt"
 
 func main() {
 	
-		  var n, sum int
-		  fmt.Scan(&n)
-		  my_slice := make([]int, n)
+	var n, sum int
+	fmt.Scan(&n)
+	my_slice := make([]int, n)
 
-		  for i:=0; i< len(my_slice); i++ {
-	        fmt.Scanf("%d", &my_slice[i])
-	      }
+	for i:=0; i< len(my_slice); i++ {
+		fmt.Scanf("%d", &my_slice[i])
+	 }
 
-	      for i:=0; i< len(my_slice); i++ {
-	         sum = sum + my_slice[i]
-	      }
+	 for i:=0; i< len(my_slice); i++ {
+	        sum = sum + my_slice[i]
+	 }
 
-	      fmt.Println(sum)
+	 fmt.Println(sum)
 }
