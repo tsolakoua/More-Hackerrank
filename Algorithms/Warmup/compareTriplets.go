2833ff6f9f202bf82976ec4cefb236fb92e5ba88
Anna Tsolakou <tsolakou.a@gmail.com>
2016-10-03 13:04:24 +0200

    Update compareTriplets.go

--- Algorithms/Warmup/compareTriplets.go
+++ Algorithms/Warmup/compareTriplets.go
@@ -3,27 +3,27 @@ import "fmt"
 
 func main() {
 
-		n := 3
-		alice := make([]int, n)
-		bob := make([]int, n)
-		var aliceTotal, bobTotal int
+	n := 3
+	alice := make([]int, n)
+	bob := make([]int, n)
+	var aliceTotal, bobTotal int
 
     	for i:=0; i< len(alice); i++ {
 	        fmt.Scanf("%d", &alice[i])
-	    }
+	}
 
-	    for i:=0; i< len(bob); i++ {
+	for i:=0; i< len(bob); i++ {
 	        fmt.Scanf("%d", &bob[i])
-	    }
+	}
 
-		for i:=0; i< len(alice); i++ {
-			if alice[i] > bob [i] {
-				aliceTotal = aliceTotal + 1
+	for i:=0; i< len(alice); i++ {
+		if alice[i] > bob [i] {
+			aliceTotal = aliceTotal + 1
 
-			} else if alice[i] < bob [i] {
+		} else if alice[i] < bob [i] {
 				bobTotal = bobTotal + 1
-			}
 		}
+	}
 
-		fmt.Print(aliceTotal, bobTotal)
+	fmt.Print(aliceTotal, bobTotal)
 }
