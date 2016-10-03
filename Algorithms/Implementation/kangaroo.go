05971dd7ca86d8b8ece054a1a37b0ffc1fe90027
Anna Tsolakou <tsolakou.a@gmail.com>
2016-10-03 13:00:25 +0200

    Update kangaroo.go

--- Algorithms/Implementation/kangaroo.go
+++ Algorithms/Implementation/kangaroo.go
@@ -5,7 +5,7 @@ func main() {
 
 	var x1, v1, x2, v2 int
 	fmt.Scanf("%d %d %d %d", &x1, &v1, &x2, &v2)
-    if v1-v2!=0 && x2 >= x1 && v1 > v2  {
+   	if v1-v2!=0 && x2 >= x1 && v1 > v2  {
 		j := (x2-x1)%(v1-v2)
 		if j == 0 {
 			fmt.Println("YES") 
