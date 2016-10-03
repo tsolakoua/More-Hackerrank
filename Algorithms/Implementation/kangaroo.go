425f935f52086aed566eff5f2feec5b03899e7ee
Anna Tsolakou <tsolakou.a@gmail.com>
2016-10-03 13:00:14 +0200

    Update kangaroo.go

--- Algorithms/Implementation/kangaroo.go
+++ Algorithms/Implementation/kangaroo.go
@@ -1,15 +1,13 @@
 package main 
 import "fmt"
-import "reflect"
 
 func main() {
 
 	var x1, v1, x2, v2 int
 	fmt.Scanf("%d %d %d %d", &x1, &v1, &x2, &v2)
     if v1-v2!=0 && x2 >= x1 && v1 > v2  {
-		j := (x2-x1)/(v1-v2)
-		fmt.Println(j)
-		if j > 0 && reflect.TypeOf(j) == reflect.TypeOf(5){
+		j := (x2-x1)%(v1-v2)
+		if j == 0 {
 			fmt.Println("YES") 
 		} else {
 			fmt.Println("NO") 
